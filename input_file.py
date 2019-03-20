301a64da0ce558d15fac72fc4166d2783bf21f7f
Luke Pitt <luke@robots.ox.ac.uk>
2019-03-20 14:27:01 +0000

    implement excel empty column analysis

--- input_file.py
+++ input_file.py
@@ -48,7 +48,33 @@ def identify_columns_maccor_excel(file_path):
     """
         Identifies columns in a maccor excel file"
     """
-    pass
+    import xlrd
+    wbook = xlrd.open_workbook(file_path, on_demand=True)
+    sheet = wbook.sheet_by_index(0)
+    column_has_data = [False for col in range(0, sheet.ncols)]
+    headers = []
+    numeric_columns = []
+    for col in range(0, sheet.ncols):
+        headers.append(sheet.cell_value(1,col))
+        if isfloat(sheet.cell_value(2,col)):
+            numeric_columns.append(col)
+        else:
+            column_has_data[col] = True
+    print(headers)
+    print(numeric_columns)
+    for sheet_id in range(0, wbook.nsheets):
+        print('Loading sheet ' + str(sheet_id))
+        sheet = wbook.sheet_by_index(sheet_id)
+        for row in range(2, sheet.nrows):
+            for column in numeric_columns[:]:
+                if float(sheet.cell_value(row,column)) != 0.0:
+                    column_has_data[column]=True
+                    numeric_columns.remove(column)
+                    print("Found data in col " + str(column)  +" : " + str(float(sheet.cell_value(row,column))))
+        print('Unloading sheet ' + str(sheet_id))
+        wbook.unload_sheet(sheet_id)
+    columns_with_data = { headers[i]: column_has_data[i] for i in range(0, len(headers)) }
+    print(columns_with_data)
 
 
 def identify_columns_maccor_text(file_type, file_path):
