ea31bdbbfbac902e3bbfd627374419725fc5b6d1
Luke Pitt <luke@robots.ox.ac.uk>
2019-05-21 13:40:38 +0100

    Add observed file path row

--- galvanalyser/database/observed_files_row.py
+++ galvanalyser/database/observed_files_row.py
@@ -133,3 +133,53 @@ class ObservedFilesRow:
                 )
                 for result in records
             ]
+
+
+class ObservedFilePathRow:
+    def __init__(
+        self,
+        monitor_path_id,
+        monitored_path,
+        observed_path,
+        monitored_for,
+        last_observed_size,
+        last_observed_time,
+        file_state=None,
+    ):
+        self.monitor_path_id = monitor_path_id
+        self.monitored_path = monitored_path
+        self.observed_path = observed_path
+        self.monitored_for = monitored_for
+        self.last_observed_size = last_observed_size
+        self.last_observed_time = last_observed_time
+        self.file_state = file_state
+
+    @staticmethod
+    def select_from_harvester_id_no_with_state(harvester_id, file_state, conn):
+        with conn.cursor() as cursor:
+            cursor.execute(
+                (
+                    "SELECT hof.monitor_path_id, hmp.path, hof.path, "
+                    "hmp.monitored_for, hof.last_observed_size, "
+                    "hof.last_observed_time "
+                    "FROM harvesters.observed_files AS hof "
+                    "INNER JOIN harvesters.monitored_paths AS hmp ON "
+                    "hof.monitor_path_id = hmp.monitor_path_id "
+                    "WHERE "
+                    "hmp.harvester_id=(%s) AND hof.file_state=(%s)"
+                ),
+                [harvester_id, file_state],
+            )
+            records = cursor.fetchall()
+            return [
+                ObservedFilePathRow(
+                    monitor_path_id=result[0],
+                    monitored_path=result[1],
+                    observed_path=result[2],
+                    monitored_for=result[3],
+                    last_observed_size=result[4],
+                    last_observed_time=result[5],
+                    file_state=file_state,
+                )
+                for result in records
+            ]
