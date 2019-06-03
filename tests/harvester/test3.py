385b1ffd178c4b7aaed6ec586922f87e5911ea52
Luke Pitt <luke@robots.ox.ac.uk>
2019-06-03 15:10:48 +0100

    fix test3

--- tests/harvester/test3.py
+++ tests/harvester/test3.py
@@ -23,14 +23,13 @@ print(end - start)
 # print(f3.get_column_to_standard_column_mapping(None))
 
 start = timer()
-data = f1.get_data_with_standard_colums(
-    {"TestTime": "TestTime", "Rec#": "Rec#", "Power": None}
-)
+data = f1.get_data_row_generator(["sample_no"])
 end = timer()
 print("f1.get_standardized_data time")
 print(end - start)
 start = timer()
-f1.write_output_file("/tmp/outputfile.csv", data)
+with open("/tmp/test3.txt", "w") as f:
+    f.writelines(data)
 end = timer()
 print("f1.write_output_file time")
 print(end - start)
