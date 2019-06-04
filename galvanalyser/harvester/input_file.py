a6bcf723dcd0b1c9e7a0cc68354eeb61d1eff72c
Luke Pitt <luke@robots.ox.ac.uk>
2019-06-04 18:01:57 +0100

    remove unnecessarry code

--- galvanalyser/harvester/input_file.py
+++ galvanalyser/harvester/input_file.py
@@ -77,84 +77,10 @@ class InputFile:
         self.metadata, self.columns_with_data, self.num_rows = load_metadata(
             self.type, file_path
         )
-        # there should be some map of loaded data, not sure whether to use file cols or standard cols
-        self.generated_columns = {}
 
     def get_test_start_date(self):
         # TODO look check file type, ask specific implementation for metadata value
         return self.metadata["Date of Test"]
-
-    def generate_missing_columns(self, possibly_missing_colums, data):
-        """
-            Recursively generate missing columns.
-            Some columns depend on the existence of others, this method
-            generates the columns in order of fewest dependencies to most
-            generating dependant columns first. Each recursion generates at
-            most one column until all required columns are generated.
-        """
-        print("generate_missing_columns")
-        print("possibly_missing_colums: " + str(possibly_missing_colums))
-        missing_colums = [
-            col
-            for col in possibly_missing_colums
-            if col not in self.generated_columns.keys()
-        ]
-        print("missing_colums: " + str(missing_colums))
-        changes_made = len(missing_colums) != len(possibly_missing_colums)
-        print("changes_made: " + str(changes_made))
-        num_rows = len(next(iter(data.values())))
-        print("num_rows: " + str(num_rows))
-        if (
-            "experiment_id" in missing_colums
-            and "experiment_id" in self.metadata
-        ):
-            self.generated_columns["experiment_id"] = [
-                self.metadata["experiment_id"]
-            ] * num_rows
-            changes_made = True
-            print("Generated missing column experiment_id")
-        elif "sample_no" in missing_colums:
-            self.generated_columns["sample_no"] = [
-                i for i in range(1, num_rows + 1)
-            ]
-            changes_made = True
-            print("Generated missing column sample_no")
-        elif "capacity" in missing_colums:
-            print("Generating missing column capacity")
-            print("len of data['amps'] is : " + str(len(data["amps"])))
-            # capacity = [0] * num_rows
-            # total = 0.0
-            # for index, value in enumerate(data["amps"]):
-            #    total += value
-            #    capacity[index] = total
-            # self.generated_columns["capacity"] = capacity
-            self.generated_columns["capacity"] = list(
-                accumulate(map(float, data["amps"]))
-            )
-            changes_made = True
-            print("Generated missing column capacity")
-        elif "power" in missing_colums:
-            power = [
-                float(data["volts"][i]) * float(data["amps"][i])
-                for i in range(num_rows)
-            ]
-            self.generated_columns["power"] = power
-            changes_made = True
-            print("Generated missing column power")
-
-        print("changes_made 2: " + str(changes_made))
-        if changes_made:
-            self.generate_missing_columns(missing_colums, data)
-        elif len(missing_colums) > 0:
-            print("Error missing these columns and unable to generate them:")
-            print(missing_colums)
-            raise battery_exceptions.DataGenerationError
-
-    def get_generatable_columns(self):
-        """
-            Returns a list of columns that can be generated
-        """
-        return ["Cyc#", "State", "DPt Time", "Step", "StepTime", "Power"]
 
     def complete_columns(self, required_columns, file_cols_to_data_generator):
         """
@@ -217,10 +143,6 @@ class InputFile:
             }
         )
         print("file_col_to_std_col 2: " + str(file_col_to_std_col))
-        #        file_col_to_std_col.update({standard_cols_to_file_cols[key]: key for
-        #                                    key in standard_cols_to_file_cols.keys()
-        #                                    if standard_cols_to_file_cols[key]
-        #                                    is not None})
 
         desired_file_cols_to_std_cols = {
             key: value
@@ -289,23 +211,6 @@ class InputFile:
                 required_column_names
             ):
                 yield "\t".join(map(str, row))
-            # while True:
-            #    yield "\t".join(
-            #        [(str(next(iterator))) for iterator in iterators]
-            #    )
-        # except StopIteration:
-        #    return
         except:
             traceback.print_exc()
             raise
-
-    def get_data_generating_missing_columns(self, required_column_names):
-        """
-            Generator that returns lists with one entry per requested column
-            Generates columns if needed
-        """
-        available_file_columns = [
-            col
-            for col in required_column_names
-            if col in self.columns_with_data
-        ]
