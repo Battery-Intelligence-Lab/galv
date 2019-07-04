8c992e500a2a97f8a4c520e961ffec96c8467a25
Luke Pitt <luke@robots.ox.ac.uk>
2019-07-04 18:06:26 +0100

    placeholder database stuff in main page

--- galvanalyser/webapp/pages/main.py
+++ galvanalyser/webapp/pages/main.py
@@ -1,16 +1,35 @@
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 from galvanalyser.webapp.pages import all_layouts
+import dash_table
 
-experiment_selector = html.Div([html.P("placeholder experiment selector")])
+from galvanalyser.database.experiment.experiments_row import ExperimentsRow
+
+def log(text):
+    with open("/tmp/log.txt", "a") as myfile:
+        myfile.write(text + "\n")
+
+experiment_selector = html.Div(
+    [
+        html.P("placeholder experiment selector"),
+        html.Button(
+            id="main_get_experiments",
+            type="button",
+            children="Get Experiments",
+        ),
+    ]
+)
 experiment_list = html.Div(
     [
         html.P("placeholder experiment list"),
         dcc.RadioItems(id="main_experiments"),
+        dash_table.DataTable(id='table',row_selectable="single",
+    columns=[{"name": i, "id": i} for i in ["id", "name","date", "type"]],
+    data=[{"hello":"aa","world":"bb","id":1}])
     ]
 )
-data_ranges = html.Div([html.P("placeholder data ranges")])
+data_ranges = html.Div([html.P("placeholder data ranges"), html.P(id="main_selected_experiment")])
 plotting_controls = html.Div([html.P("placeholder plotting_controls")])
 
 layout = html.Div(
@@ -25,20 +44,30 @@ layout = html.Div(
 
 all_layouts.append(layout)
 
-# def add_layouts(layouts):
-#    layouts.append(layout)
-
 
 def register_callbacks(app, config):
     @app.callback(
-        Output("app-1-display-value", "children"),
-        [Input("app-1-dropdown", "value")],
+        Output("table", "data"),
+        [Input("main_get_experiments", "n_clicks")]
     )
-    def display_value(value):
-        return 'You have selected "{}"'.format(value)
-
+    def get_experiments(n_clicks):
+        log("in get_experiments")
+        options = []
+        if n_clicks:
+            try:
+                conn = config["get_db_connection_for_current_user"]()
+                experiments = ExperimentsRow.select_all_experiments(conn)
+                options = [{"id": exp.id,
+                            "name": exp.name, "date": exp.date, "type": exp.experiment_type} for exp in experiments]
+            finally:
+                conn.close()
+        return options
+    
     @app.callback(
-        [Output("url", "pathname")], [Input("page2-button", "n_clicks")]
+        Output("main_selected_experiment", "children"),
+        [ Input("table", "selected_row_ids")]
     )
-    def login_handler(n_clicks):
-        return False, "/login"
+    def experiment_selected(selected_row_ids):
+        return f"Selected: {selected_row_ids}"
+
+
