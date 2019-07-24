50cbb75395871f44b6786d881d08dd52079f1f67
Luke Pitt <luke@robots.ox.ac.uk>
2019-07-24 18:30:06 +0100

    populate members based on protobuf lists

--- webapp-static-content/js/data-range.js
+++ webapp-static-content/js/data-range.js
@@ -2,6 +2,8 @@
 goog.module('datarange');
 goog.module.declareLegacyNamespace();
 
+goog.require('proto.galvanalyser.DataRange');
+goog.require('proto.galvanalyser.DataRanges');
 goog.require('goog.structs.AvlTree');
 
 class DataRange{
@@ -15,10 +17,31 @@ function data_range_comp(a, b){
     return b.from - a.from;
 }
 
+class ReadingData{
+    constructor(column_name){
+        this.column = column_name;
+        this.tree = new goog.structs.AvlTree(data_range_comp);
+    }
+
+}
+
 class ExperimentData{
 
     constructor(){
-        this.tree = new goog.structs.AvlTree(data_range_comp);
+        let temp_pb = new proto.galvanalyser.DataRange();
+        let list_regex = /get(\w+)List/;
+        this.columns = [];
+        for(const prop in temp_pb){
+            if (!temp_pb.hasOwnProperty(prop)) {
+                let match = list_regex.exec(prop);
+                if(match && match[1]){
+                    let column = match[1];
+                    this[column] = new ReadingData(column);
+                    this.columns.push(column);
+                    //console.log(`obj.${prop} = ${temp_pb[prop]}`);
+                }
+              }
+        }
     }
 }
 
