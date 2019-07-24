314f16bc56e19b790a3543ad1c6e840ca43afe01
Luke Pitt <luke@robots.ox.ac.uk>
2019-07-24 16:38:35 +0100

    Add datarange placeholder file

---
+++ webapp-static-content/js/data-range.js
@@ -0,0 +1,26 @@
+//"use strict";
+goog.module('datarange');
+goog.module.declareLegacyNamespace();
+
+goog.require('goog.structs.AvlTree');
+
+class DataRange{
+    constructor(data_indicies, data_values){
+        this.from = 0;
+        this.to = 1;
+    }
+}
+
+function data_range_comp(a, b){
+    return b.from - a.from;
+}
+
+class ExperimentData{
+
+    constructor(){
+        this.tree = new goog.structs.AvlTree(data_range_comp);
+    }
+}
+
+exports.DataRange = DataRange;
+exports.ExperimentData = ExperimentData;
