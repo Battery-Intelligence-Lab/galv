255e0b4e7958772b72c5079ff6b79e166da37aa0
Luke Pitt <luke@robots.ox.ac.uk>
2019-07-30 17:32:51 +0100

    adjust formatting

--- webapp-static-content/js/data-range.js
+++ webapp-static-content/js/data-range.js
@@ -6,41 +6,41 @@ goog.require('proto.galvanalyser.DataRange');
 goog.require('proto.galvanalyser.DataRanges');
 goog.require('goog.structs.AvlTree');
 
-class DataRange{
-    constructor(data_indicies, data_values){
+class DataRange {
+    constructor(data_indicies, data_values) {
         this.from = 0;
         this.to = 1;
     }
 }
 
-function data_range_comp(a, b){
+function data_range_comp(a, b) {
     return b.from - a.from;
 }
 
-class ReadingData{
-    constructor(column_name){
+class ReadingData {
+    constructor(column_name) {
         this.column = column_name;
         this.tree = new goog.structs.AvlTree(data_range_comp);
     }
 
 }
 
-class ExperimentData{
+class ExperimentData {
 
-    constructor(){
+    constructor() {
         let temp_pb = new proto.galvanalyser.DataRange();
         let list_regex = /get(\w+)List/;
         this.columns = [];
-        for(const prop in temp_pb){
+        for (const prop in temp_pb) {
             if (!temp_pb.hasOwnProperty(prop)) {
                 let match = list_regex.exec(prop);
-                if(match && match[1]){
+                if (match && match[1]) {
                     let column = match[1];
                     this[column] = new ReadingData(column);
                     this.columns.push(column);
                     //console.log(`obj.${prop} = ${temp_pb[prop]}`);
                 }
-              }
+            }
         }
     }
 }
