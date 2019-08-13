56c8b787be7d1f8f1b612d11791ff7f8c2c637a4
Luke Pitt <luke@robots.ox.ac.uk>
2019-08-13 15:55:36 +0100

    WIP

--- galvanalyser/webapp/assets/update-graph.js
+++ galvanalyser/webapp/assets/update-graph.js
@@ -2,6 +2,7 @@
 
 var graph_state = {};
 var graph_traces = [];
+var all_experiment_data = new Map();//new datarange.ExperimentData();
 
 goog.require('proto.galvanalyser.DataRanges');
 goog.require('datarange');
@@ -24,20 +25,46 @@ window.dash_clientside.clientside_graph = {
                     //var byteArray = new Uint8Array(arrayBuffer);
                     let message = proto.galvanalyser.DataRanges.deserializeBinary(arrayBuffer);
                     let ranges_list = message.getRangesList();
-                    console.log(ranges_list);
-                    if (!('dataplot' in graph_state)) {
-                        alert("Adding data plot")
-                        let trace1 = {
-                            x: ranges_list[0].getTestTimeList(),
-                            y: ranges_list[0].getVoltsList(),
-                            mode: 'markers',
-                            type: 'scattergl'
-                        };
-                        graph_state.dataplot = trace1;
-                        Plotly.addTraces(plot, trace1);
-                    } else {
-                        alert("Not adding data plot");
+                    let experment_id = message.getExperimentId();
+                    if(! all_experiment_data.has(experment_id)){
+                        all_experiment_data.set(experment_id, new datarange.ExperimentData());
                     }
+                    let experiment_data = all_experiment_data.get(experment_id);
+                    ranges_list.forEach(function(range){
+                        experiment_data.add_protobuf_data_range(range);
+                    });
+                    console.log(ranges_list);
+                    ranges_list.forEach(function(range){
+                        let volt_list = range.getVoltsList();
+                        let data_name = `${message.getExperimentId()}_${range.getStartSampleNo()}_${volt_list.length}_volts`;
+                        if (!(data_name in graph_state)) {
+                            alert("Adding data plot")
+                            let trace = {
+                                x: range.getTestTimeList(),
+                                y: volt_list,
+                                mode: 'markers',
+                                type: 'scattergl'
+                            };
+                            graph_state[data_name] = trace;
+                            Plotly.addTraces(plot, trace);
+                        } else {
+                            alert("Not adding data plot");
+                        }
+                    });
+                    
+                    //if (!('dataplot' in graph_state)) {
+                    //    alert("Adding data plot")
+                    //    let trace1 = {
+                    //        x: ranges_list[0].getTestTimeList(),
+                    //        y: ranges_list[0].getVoltsList(),
+                    //        mode: 'markers',
+                    //        type: 'scattergl'
+                    //    };
+                    //    graph_state.dataplot = trace1;
+                    //    Plotly.addTraces(plot, trace1);
+                    //} else {
+                    //    alert("Not adding data plot");
+                    //}
                 }
             };
 
@@ -51,19 +78,19 @@ window.dash_clientside.clientside_graph = {
             // if(! (current_trace['id'] in data_ids)) -- we need to make a list of ids
         }
 
-        if (!('first' in graph_state)) {
-            alert("Adding plot")
-            let trace1 = {
-                x: [1, 2, 3, 4],
-                y: [10, 15, 13, 17],
-                mode: 'markers',
-                type: 'scattergl'
-            };
-            graph_state.first = trace1;
-            Plotly.addTraces(plot, trace1);
-        } else {
-            alert("Not adding plot");
-        }
+        //if (!('first' in graph_state)) {
+        //    alert("Adding plot")
+        //    let trace1 = {
+        //        x: [1, 2, 3, 4],
+        //        y: [10, 15, 13, 17],
+        //        mode: 'markers',
+        //        type: 'scattergl'
+        //    };
+        //    graph_state.first = trace1;
+        //    Plotly.addTraces(plot, trace1);
+        //} else {
+        //    alert("Not adding plot");
+        //}
         return "";
     },
     login_refresh: function(children) {
@@ -73,4 +100,4 @@ window.dash_clientside.clientside_graph = {
         return "";
     }
 
-}
+};
