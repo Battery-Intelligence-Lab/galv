8c6eacf90d10ca0b4e9b6f2e3ec6ed7f74d775f7
Luke Pitt <luke@robots.ox.ac.uk>
2019-08-27 17:08:43 +0100

    echo build steps

--- builder/build.sh
+++ builder/build.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-
+echo "
+Running docker-builder build.sh
+"
 source /workdir/venv/bin/activate
 
 cd /workdir/project
 
+echo "
+make init:
+"
 make init
 
+echo "
+npm install:
+"
 pushd libs/galvanalyser-dash-components
 npm install
 popd
 
+echo "
+make custom-dash-components:
+"
 make custom-dash-components
+
+echo "
+make protobuf:
+"
 
 make protobuf
