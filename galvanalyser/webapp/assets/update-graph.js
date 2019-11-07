87bbe6901a9d6f2a5d9501cab5e7afba9ea48dc8
Luke Pitt <luke@robots.ox.ac.uk>
2019-11-07 13:08:19 +0000

    use array instead of map for legend entries

--- galvanalyser/webapp/assets/update-graph.js
+++ galvanalyser/webapp/assets/update-graph.js
@@ -178,7 +178,7 @@ function update_graph() {
         let plot = document.getElementById('main-graph');
         //update graph
         let traces = [];
-        let legend_entries = new Map();
+        let legend_entries = [];
         for (const [requested_dataset_id, requested_dataset_data] of requested_metadata_ranges) {
             if (all_dataset_data.has(requested_dataset_id)) {
                 let available_dataset_data = all_dataset_data.get(requested_dataset_id);
@@ -194,15 +194,16 @@ function update_graph() {
                     if (available_dataset_data.columns.has(requested_column_id)) {
                         let available_column_reading_data = available_dataset_data.columns.get(requested_column_id);
                         for (const requested_data_range of requested_reading_data) {
-                            let legend_entry_id = `${requested_dataset_id}_${requested_column_id}_range_from_range_to`
-                            legend_entries[legend_entry_id] = {
+                            let legend_entry_id = `${requested_dataset_id}_${requested_column_id}_${requested_data_range.from}_${requested_data_range.to}`
+                            legend_entries.push({
+                                legend_entry_id: legend_entry_id,
                                 range_name: `${requested_dataset_id} ${column_names[requested_column_id]}`,
                                 colour: '0x0000FF',
                                 dataset_id: requested_dataset_id,
                                 column_id: requested_column_id,
                                 column_name: column_names[requested_column_id],
                                 requested_data_range: requested_data_range
-                            }
+                            });
                             let available_y_ranges = available_column_reading_data.get_ranges_between(requested_data_range.from, requested_data_range.to);
                             for (const available_y_range of available_y_ranges) {
                                 let available_x_ranges = sample_time_data.get_ranges_between(available_y_range.from, available_y_range.to);
