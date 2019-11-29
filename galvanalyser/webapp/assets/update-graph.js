91b4b713156535aacd8c7cfcecf75cb57dd46b84
Luke Pitt <luke@robots.ox.ac.uk>
2019-11-29 14:31:25 +0000

    remove dead code

--- galvanalyser/webapp/assets/update-graph.js
+++ galvanalyser/webapp/assets/update-graph.js
@@ -1,6 +1,5 @@
 "use strict";
 
-var graph_state = new Map();
 var requested_metadata_ranges = new Map();
 var requested_graph_ranges = new Map();
 var graph_traces = [];
@@ -287,103 +286,7 @@ window.dash_clientside.clientside_graph = {
         requested_graph_ranges = new_config;
         requested_metadata_ranges = new_dataset_ranges;
         update_graph();
-        //for (const row of data) {
-        //    let foo = new datarange.DatasetData();
-        //
-        //    let oReq = new XMLHttpRequest();
-        //    //oReq.open("GET", `/dataset/${row.dataset}/data?from=${row.samples_from}&to=${row.samples_to}&column=test_time,volts,amps`, true);
-        //    oReq.open("GET", `/dataset/${row.dataset}/data?from=${row.samples_from}&to=${row.samples_to}&column=${row.column}`, true);
-        //    oReq.responseType = "arraybuffer";
-        //
-        //    oReq.onload = function(oEvent) {
-        //        let arrayBuffer = oReq.response; // Note: not oReq.responseText
-        //        if (arrayBuffer) {
-        //            //var byteArray = new Uint8Array(arrayBuffer);
-        //            let message = proto.galvanalyser.DataRanges.deserializeBinary(arrayBuffer);
-        //            let ranges_list = message.getRangesList();
-        //            let dataset_id = message.getDatasetId();
-        //            //if(! all_dataset_data.has(dataset_id)){
-        //            //    all_dataset_data.set(dataset_id, new datarange.DatasetData());
-        //            //}
-        //            //let dataset_data = all_dataset_data.get(dataset_id);
-        //            //ranges_list.forEach(function(range){
-        //            //    dataset_data.add_protobuf_data_range(range);
-        //            //});
-        //            ////console.log(ranges_list);
-        //            //ranges_list.forEach(function(range){
-        //            //    let volt_list = range.getVoltsList();
-        //            //    let data_name = `${message.getDatasetId()}_${range.getStartSampleNo()}_${volt_list.length}_volts`;
-        //            //    if (!(data_name in graph_state)) {
-        //            //        alert("Adding data plot")
-        //            //        let trace = {
-        //            //            x: range.getTestTimeList(),
-        //            //            y: volt_list,
-        //            //            mode: 'markers',
-        //            //            type: 'scattergl'
-        //            //        };
-        //            //        graph_state[data_name] = trace;
-        //            //        Plotly.addTraces(plot, trace);
-        //            //    } else {
-        //            //        alert("Not adding data plot");
-        //            //    }
-        //            //});
-        //            
-        //            //if (!('dataplot' in graph_state)) {
-        //            //    alert("Adding data plot")
-        //            //    let trace1 = {
-        //            //        x: ranges_list[0].getTestTimeList(),
-        //            //        y: ranges_list[0].getVoltsList(),
-        //            //        mode: 'markers',
-        //            //        type: 'scattergl'
-        //            //    };
-        //            //    graph_state.dataplot = trace1;
-        //            //    Plotly.addTraces(plot, trace1);
-        //            //} else {
-        //            //    alert("Not adding data plot");
-        //            //}
-        //        }
-        //    };
-        //
-        //    oReq.send(null);
-        //}
-        //
-        //let plot = document.getElementById('main-graph');
-        //
-        //for (const current_trace of graph_traces) {
-        //    ////console.log(val);
-        //    // if(! (current_trace['id'] in data_ids)) -- we need to make a list of ids
-        //}
-
-        //if (!('first' in graph_state)) {
-        //    alert("Adding plot")
-        //    let trace1 = [{
-        //        x: [1, 2, 3, 4],
-        //        y: [10, 15, 13, 17],
-        //        mode: 'markers',
-        //        type: 'scattergl'
-        //    }];
-        //    graph_state.first = trace1;
-        //    Plotly.react(plot, trace1);
-        //} else {
-        //    alert("Updating plot");
-        //    let traces = [{
-        //        x: [1, 2, 3, 4],
-        //        y: [-10, 15, -13, 17],
-        //        mode: 'markers',
-        //        type: 'scattergl'
-        //    },
-        //    {
-        //        x: [1, 2, 3, 4],
-        //        y: [15, 10, -3, 7],
-        //        mode: 'markers',
-        //        type: 'scattergl'
-        //    }];
-        //    if(Math.random() < 0.5){
-        //        traces = [traces[1]];
-        //    }
-        //    //graph_state.first = trace1;
-        //    Plotly.react(plot, traces);
-        //}
+        
         return "";
     },
     login_refresh: function(children) {
