e0b45200f23e3770ef9bf868de847da6631424c3
martinjrobins <martinjrobins@gmail.com>
2022-02-16 09:46:28 +0000

    #23 remove MRG license and replace with BSD2

--- backend/galvanalyser/harvester/biologic_input_file.py
+++ backend/galvanalyser/harvester/biologic_input_file.py
@@ -1,3 +1,7 @@
+# SPDX-License-Identifier: BSD-2-Clause
+# Copyright  (c) 2020, The Chancellor, Masters and Scholars of the University
+# of Oxford, and the 'Galvanalyser' Developers. All rights reserved.
+
 import os
 import ntpath
 import copy
